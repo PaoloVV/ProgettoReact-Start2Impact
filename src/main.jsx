580167720cabe720d451e2ad87eb90fb944fe9e7
PaoloVV <118202060+PaoloVV@users.noreply.github.com>
2024-01-16 18:50:40 +0100

    update hashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,16 @@ import Home from './Home.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.js'
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements, createHashRouter } from 'react-router-dom'
 import ErrorPage from './assets/pages/ErrorPage.jsx'
 import RandomRecipes from './assets/components/RandomRecipes.jsx'
 import AdvancedSearch from './assets/pages/AdvancedSearch.jsx'
 import RecipeDetails from './assets/components/RecipeDetails.jsx'
 
-const router = createBrowserRouter(
+const router = createHashRouter(
   createRoutesFromElements(
     <>
-      <Route path='/ProgettoReact-Start2Impact' element={<Home />}></ Route>
+      <Route path='/' element={<Home />}></ Route>
       <Route path='/random' element={<RandomRecipes />} />
       <Route path='/advanced' element={<AdvancedSearch />} />
       <Route path='/advanced/:id' element={<RecipeDetails />} />
